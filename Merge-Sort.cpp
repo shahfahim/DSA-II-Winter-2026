173b11e075be449a5c42ba38534d41f6e524bc1d
shahfahim <72489234+shahfahim@users.noreply.github.com>
2026-03-04 01:10:23 +0600

    Add files via upload

---
+++ Merge-Sort.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+        int i = m - 1;          // pointer for nums1
+        int j = n - 1;          // pointer for nums2
+        int k = m + n - 1;      // position to fill
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        // Copy remaining nums2 elements
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+};
